e7992285059c63c94930b3cd5708188a611a1a2b
Anish Kanti Ghorai <101464183+GhostRider1122@users.noreply.github.com>
2024-04-21 21:04:20 +0000

    pattern 4 done

---
+++ patterns/Python/pattern4.py
@@ -0,0 +1,22 @@
+"""
+Generate the following pattern
+1
+2 2
+3 3 3
+4 4 4 4
+5 5 5 5 5
+"""
+
+def pattern3(n):
+    for i in range(1,n+1):
+        for j in range(i):
+            print(i," ", end="")
+        print("")
+        
+def main():
+    i = int(input("Enter the number of rows: "))
+    print("Here is the generated pattern\n")
+    pattern3(i)
+    
+if __name__ == "__main__":
+    main()
